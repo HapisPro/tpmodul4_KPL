d3991ed225bb69d985d52078d796d21c04ab7f24
alkausarhapis <alkausarhapis@gmail.com>
2025-03-09 10:49:51 +0700

    menambahkan implementasi dengan table-driven

--- tpmodul4_103022300069/Program.cs
+++ tpmodul4_103022300069/Program.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using tpmodul4_103022300069;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Soal 1 (Table Driven):");
+        foreach (KodePos.Kelurahan kel in Enum.GetValues(typeof(KodePos.Kelurahan)))
+        {
+            Console.WriteLine(kel + " : " + KodePos.GetKodePos(kel));
+        }
     }
 }
